40057cc20c78154a0b4bebc27d01e25b5d308b81
Richard McAlexander <richardmcalexander@MacBook-Pro.fios-router.home>
2020-06-05 14:53:36 -0400

    added import

--- code/data_manage.R
+++ code/data_manage.R
@@ -1,1 +1,9 @@
-hk
+#LOAD PACKAGES
+library(tidyverse)
+
+#IMPORT DATA. JUST US FOR NOW
+library(readxl)
+navco_US <- read_excel("~/Dropbox/concessions_resist/data/navco3-0countrysheets/NAVCO3.0_US.xlsx")
+
+
+View(NAVCO3_0_US)
